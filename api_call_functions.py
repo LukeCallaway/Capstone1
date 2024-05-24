b8d8c769438d253ee1e3a464fc41277b45ee77af
Luke Callaway <lacallaway98@gmail.com>
2024-05-23 22:47:13 -0500

    added is_on_list function

--- api_call_functions.py
+++ api_call_functions.py
@@ -3,26 +3,16 @@ from my_secrets import MY_API_KEY
 
 base_url =  'https://api.watchmode.com/v1/'
 
-def get_basic_info(search_value, search_type = None, search_field='name'):
+def get_basic_info(search_value, search_type = 3, search_field='name'):
     """get basic info of movie searching by actor name or movie title"""
 
-    params = {'search_field': search_field, 'search_value': search_value, 'apiKey':  MY_API_KEY}
-    full_url = base_url
+    params = {'search_field': search_field, 'search_value': search_value, 'search_type': search_type,  'apiKey':  MY_API_KEY}
 
-    if search_type != None:
-        full_url += search_type
+    full_url = base_url + 'autocomplete-search/'
 
     res = requests.get(full_url, params=params)
 
     return res.json()
-
-
-    # for result in data['title_results']:
-    #     print(result['name'], result['year'], result['type'], 'end of result')
-
-    # data['title_results'][0] <- data['title_results'] gives arrary of results
-    # title details page will be most request
-
 
 # can only search api by id
 def get_all_info(id):
@@ -31,3 +21,11 @@ def get_all_info(id):
 
     res = requests.get(full_url, params=params)
     return res.json()
+
+def is_on_list(u_id, m_id, db):
+    movie = (db.query.filter(db.user_id == u_id,
+                                            db.movie_id == m_id)
+                                            .all())
+    if(len(movie) > 0):
+        return True
+    return False
